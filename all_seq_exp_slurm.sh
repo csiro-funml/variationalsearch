de512a21ef9df7790f2f98ee2574088057f839ed
Dan Steinberg <daniel.steinberg@data61.csiro.au>
2024-07-12 12:06:45 +1000

    hpc run scripts

---
+++ all_seq_exp_slurm.sh
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:70864f4d8c7f89c03355568f978497995a3c3443f42f3e001b7c56476afe94f2
+size 130
---
+++ run_seq_exp_hpc.sh
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2293a0e051ffed0ffb972985585940578b7820d1014a1172689e60ff5479cc79
+size 385
