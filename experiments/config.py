b9715fe27fc21eed02956d1dc10dcef29efdaa73
dsteinberg <dan.m.steinberg@gmail.com>
2024-07-12 12:44:06 +1000

    retrained AAV surrogate

--- experiments/config.py
+++ experiments/config.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8a05d735c4b04f6d1cc97bfd58bdb321559556afde6575ad9e130b56c3b12f66
-size 7388
+oid sha256:ec146e707aa6212692df9465a55b385dc93a6f34ddfe224ab1e5df3810030471
+size 7366
