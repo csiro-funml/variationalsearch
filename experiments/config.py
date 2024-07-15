03a495fc09565b9b7bf84bd2a9d1531729904330
dsteinberg <dan.m.steinberg@gmail.com>
2024-07-16 09:38:45 +1000

    fix config

--- experiments/config.py
+++ experiments/config.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d3a8d5c58ed2c8289ab565c9bbe5bdabc18723eecf9743d42362fd75e74ce233
-size 7937
+oid sha256:d79e39f5b81780d7e7a5c94758d6f9075b24d861b183847e351de43a29726b5a
+size 7939
