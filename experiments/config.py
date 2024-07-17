b5ad82845b0582cb5ee21a92008de5d8529e0e7a
dsteinberg <dan.m.steinberg@gmail.com>
2024-07-17 13:28:40 +1000

    rnn priors

--- experiments/config.py
+++ experiments/config.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9d8adf610a5e15e1c998f7f64980a5c9508fdd5fc7fb94994f333fb3105b5a55
-size 8134
+oid sha256:bf20162f481a6c10909a21046dc63816d311994b3b2d56c01e6cb830ca8db018
+size 8345
