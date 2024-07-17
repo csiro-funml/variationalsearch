df2fd67681ec04f0627a93de33edb58099bbbd9c
dsteinberg <dan.m.steinberg@gmail.com>
2024-07-17 10:27:56 +1000

    RNN proposal

--- vsd/proposals.py
+++ vsd/proposals.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1b0796bb2eca267fc360a64d625d75d16647049c85a50be9aea2fac2c13bf04a
-size 7324
+oid sha256:c52618c450bc9be442f58f1b84d2705cc34f531d9a5f28eb7b0f6659b0b6d2fa
+size 8386
