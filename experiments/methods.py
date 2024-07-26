612b35512f29c5b58ff5f17dc710fb7379eb5556
dsteinberg <dan.m.steinberg@gmail.com>
2024-07-26 16:27:17 +1000

    making sure prior and proposal are compatible if loading from prior

--- experiments/methods.py
+++ experiments/methods.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0082ff0ec7e0da0835f14e526b6bcad7ca8bae523317ce19a6744cd885678ea8
-size 10456
+oid sha256:99a763058cf27d4c8fb0cae509a1a8ffa4ea6232116d19ebb5b91a0f3a22a42b
+size 10919
