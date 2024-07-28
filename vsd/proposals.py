83918f69d5207711a16972856de5eaa77f7710bc
dsteinberg <dan.m.steinberg@gmail.com>
2024-07-28 17:26:11 +1000

    give T-CNN one more hidden layer

--- vsd/proposals.py
+++ vsd/proposals.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:964b98d123c34280a5e3007eb959e770ad70f96e54436bf61826b76b797af2c5
-size 12301
+oid sha256:7e3a221b361db1a2ffa6f0ea2b5ec75e5707ad365577e77810c67886e394d27f
+size 12529
