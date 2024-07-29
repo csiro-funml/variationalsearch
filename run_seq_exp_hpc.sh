a012786d6541ffe7d830e36ab5ddac1f60757b81
Dan Steinberg <daniel.steinberg@data61.csiro.au>
2024-07-29 19:26:21 +1000

    more realisting running time with slurm

--- run_seq_exp_hpc.sh
+++ run_seq_exp_hpc.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e104674322521be7b068ade267f08bbf5c1550f2df4d246ff3dbe89067ddef7d
-size 468
+oid sha256:12a592fbdcdd370da94a495383c43b4a313728b2ed09584a15c8362ac393a92c
+size 469
