df8330ff6fdd7ce46c058f04874812eacc91d396
dsteinberg <dan.m.steinberg@gmail.com>
2024-07-29 16:46:22 +1000

    better implementation of RemovePadding that is device independent

--- vsd/utils.py
+++ vsd/utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f9438acf6d242b82966ccd51af77c664f191517e2ee5add80b8f9284e5487307
-size 2374
+oid sha256:b470452f228671c5d157c474265c1d3e1e52eabb56720604d6374f4e06ab7b7e
+size 2272
