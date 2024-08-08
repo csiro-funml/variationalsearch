7399dffae784b02bf6eb8615f0f2adbaa40c52b2
dsteinberg <dan.m.steinberg@gmail.com>
2024-08-08 10:37:09 +1000

    reimplement T-TFME proposal

--- abl_seq_exp_slurm.sh
+++ abl_seq_exp_slurm.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f8a7be995b905d0729223e07b00c1f8ceb29bdf6cf0012bb46725587d107cfb9
-size 124
+oid sha256:bae9d0f7eb81f5afac80dae74ac97e83be41adb144815b44f472ca9242292531
+size 131
--- vsd/proposals.py
+++ vsd/proposals.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8020a2e982b12731e7f26d568ee8ea18ebf768e6f15e40981703e1ff93d0cd12
-size 12743
+oid sha256:efcd82e64173f15ee86839a429cacffeedc19e9192d5f268ec916ce15d1a8f56
+size 13754
