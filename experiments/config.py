add897eed954ba3d40be9a6ff3699a907619814e
dsteinberg <dan.m.steinberg@gmail.com>
2024-08-26 17:03:25 +1000

    fix adalead config error

--- experiments/config.py
+++ experiments/config.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3fa72ef3629fe65066e19e9db829197c64f42b85412c1677d1fffa617673b646
-size 13085
+oid sha256:bfecd5e0a64f3598f52d2eb58a8025f31fcb8a85894bea5948436c0da45f1cb3
+size 13083
