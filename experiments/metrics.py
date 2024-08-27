fb1d7e42928ff6859ddec5c23a4383bffc6702a5
dsteinberg <dan.m.steinberg@gmail.com>
2024-08-27 17:35:07 +1000

    change def of performance

--- experiments/metrics.py
+++ experiments/metrics.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e64890de5e664ab9bf7840be1ed29771c12e554cf102a3721819791166e33b7a
-size 3757
+oid sha256:79572a349258baea615bad08d7654417d9d512e1fe5b90c3d35e038f5ee463cb
+size 3749
