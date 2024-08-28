c5ba4ac61437772c055dabf0c1e428abcd35a4f3
dsteinberg <dan.m.steinberg@gmail.com>
2024-08-28 21:10:04 +1000

    mods to recall, save labels

--- experiments/metrics.py
+++ experiments/metrics.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:79572a349258baea615bad08d7654417d9d512e1fe5b90c3d35e038f5ee463cb
-size 3749
+oid sha256:eccae123855b11d9cc5b3d5feeda35ef1c6f245dd556bf61de082ad901ec89c1
+size 3765
--- experiments/scripts/batch_rounds.py
+++ experiments/scripts/batch_rounds.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8ecba03c1f6a125ec4a9d815366532b46a14c99ef2514219c03479055cf22843
-size 10572
+oid sha256:78361cdf5b5796bdffb0ca523cb48cbd7eca6119a5b73afafc4d137cc40ae7bc
+size 11204
