48d39fc8079d866d8ae0f0121bfbc5541bc949f4
dsteinberg <dan.m.steinberg@gmail.com>
2024-09-01 09:19:37 +1000

    update GFP prior

--- experiments/scripts/train_prior.py
+++ experiments/scripts/train_prior.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5dcc8b54be8b76ab7f22ef71fd9c1c2606cdf093f883d8a8364eb23e2806b649
-size 4499
+oid sha256:2257f825c96b94bfaf2e3b48100fb53fe8c3f5f4ecc73d3c5723d9cc3c742bce
+size 3861
