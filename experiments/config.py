09384388afd7772a865fc1ceed26a2f6ab9600c6
dsteinberg <dan.m.steinberg@gmail.com>
2024-09-01 09:03:19 +1000

    fine tuning GFP CPE experiment

--- experiments/config.py
+++ experiments/config.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d45fa023819b9269b1bbba089beef8954d2611b08f4f20a24c1277e1b70c08e7
-size 12416
+oid sha256:3f448d4abedc9a75d884ab8e3431bed604c9d16de1fc5c56b7fb4fb01752d233
+size 12398
