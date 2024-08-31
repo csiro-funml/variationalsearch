a872fc655b0a6cd1bf8ac73a5647923ce6d1e9a5
dsteinberg <dan.m.steinberg@gmail.com>
2024-08-31 18:16:25 +1000

    remove stop options from GP surrogates

--- experiments/scripts/batch_rounds.py
+++ experiments/scripts/batch_rounds.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:03ee40787c1bd3ca694fbb3c2cf4bd228c0381039196ed3a4fbbb1338ce4c4cd
-size 11208
+oid sha256:97ee24f23e9257a51570bbe0a2da3cf2b1b0d6d983f86c7514341a35129c6e0d
+size 11150
--- vsd/surrogates.py
+++ vsd/surrogates.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:400bdfc4ecb8e6e796946dac8f9e7cf97c70722a1d6711b8181a50ccf6764d1f
-size 9005
+oid sha256:b42ae8debb6539c2e979c4c0d00f39aeb8b430686929bf9575c4399137f45165
+size 9009
