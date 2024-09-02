aa07e6ad7553626291301eb2bb69d8746a9cf7c3
dsteinberg <dan.m.steinberg@gmail.com>
2024-09-02 18:25:00 +1000

    better AAV and GFP cpes

--- experiments/config.py
+++ experiments/config.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:50513e88c97a5dd8abc2758ea7b4892f66b754d8ad997c7ab1fa76dcc6164e3e
-size 12810
+oid sha256:cf08da450a1df3ca1e2d9381f282f15be1255d651625025e0721c55042dd26f4
+size 12905
