c78ce31c3e6140ae3c0e622a458ebf6205bae87f
dsteinberg <dan.m.steinberg@gmail.com>
2024-09-16 09:53:05 +1000

    improved CNN surrogate, digits caching

--- experiments/scripts/digits.py
+++ experiments/scripts/digits.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7a9a18c5e2ced59a46b3b53944bafa38fb1b2f19a816cfaca48fd2f4e47c3acf
-size 9073
+oid sha256:8ceeac689e96ac5a9718820e070fa14115f5883a7f94cccfb11fa33067ca0945
+size 10837
--- vsd/surrogates.py
+++ vsd/surrogates.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1ab99bc6d60858509aa795ea60823bff3454a6125843a91bbf86aeaac9b6e200
-size 9788
+oid sha256:6550641068a8e77ae2305a041a34ad9d9c2b82ab7bd233cb54da4950d9331ff6
+size 10100
