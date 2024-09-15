e3b95edb263be8caec053986fae9f61277c97101
dsteinberg <dan.m.steinberg@gmail.com>
2024-09-15 13:14:07 +1000

    trainbug in eda and reinforce with LSTM

--- vsd/generation.py
+++ vsd/generation.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4216a0c14610895f77d5be5559f32669712e03ece63c74106c43692720dfc86e
-size 18185
+oid sha256:c56fcbd0588ab3ca87dc58b03a0d7fd7a2c92b01e1e09263f44d6cd218e3e587
+size 18321
--- vsd/proposals.py
+++ vsd/proposals.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:662c6cecf78e83277b25717965f6a30f886bd03171f8ba41f45c383a9b288f4f
-size 23169
+oid sha256:41ec097ce43fd7fa43855a866247e19ee5d35c81db9472942c72a0ad20e031ee
+size 20906
