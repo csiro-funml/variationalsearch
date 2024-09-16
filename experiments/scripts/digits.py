0297771b68ce472fc74527cd538166b191419f6e
dsteinberg <dan.m.steinberg@gmail.com>
2024-09-16 17:05:32 +1000

    maybe too much gradient clipping

--- experiments/scripts/digits.py
+++ experiments/scripts/digits.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:63eea28c2575c9e72a33cd4c3cdd9008cf37e7460b4513b4465fa9ee3197dd84
-size 11531
+oid sha256:0f521e83338b925976e61f23e988c342c7d803466e2c47c33945c3d47aefdf89
+size 11855
--- experiments/scripts/plot_results.copy.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:07543ee055873c0858906ab3754642273bbb47b3473620239bbd7a20ea8023ee
-size 2601
