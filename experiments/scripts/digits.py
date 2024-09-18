6bbdcc44fd9cf49b7b3ff56f3fa063bd5e0ba228
dsteinberg <dan.m.steinberg@gmail.com>
2024-09-19 07:17:13 +1000

    shallower LSTM

--- experiments/scripts/digits.py
+++ experiments/scripts/digits.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6441454a6317c39255458933d42771ebbaa9c633aa48b628c9a0feddccec02f2
-size 12036
+oid sha256:4b53c8fb9c4f427f72c4c3feb424926cf0504d167ae87869ffdd7e332509bbac
+size 12388
