8728ffa02f7fcc6ec9f827343c3584df13f7db6a
dsteinberg <dan.m.steinberg@gmail.com>
2024-09-18 16:41:22 +1000

    keep digits logs separate

--- experiments/scripts/digits.py
+++ experiments/scripts/digits.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c7221542baff8c1ab6ed2c741dbb1c7b3cdcb9de1cefe0eb49f4f230b632df8e
-size 11985
+oid sha256:d1e04d9bf72b6a131c4916bd022f0130465453dd9de4b704b8a853cb42101a2a
+size 11998
