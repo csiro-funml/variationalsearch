6d3d0be5a875b18a4d7a75451427d35112b56707
dsteinberg <dan.m.steinberg@gmail.com>
2024-09-18 16:06:33 +1000

    better cpes

--- experiments/config.py
+++ experiments/config.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2ddb8548e6722bd988081bd57ef5388cf214bbd281712b180f443935098f3228
-size 13654
+oid sha256:c264da2ff186df0b28e018a6adc1e815154408f1d013bd949ad69da76abfb695
+size 13305
