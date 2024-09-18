9264c47466b440837a2ea57907b90dceaeca8990
dsteinberg <dan.m.steinberg@gmail.com>
2024-09-18 22:18:55 +1000

    AAV transformer

--- experiments/config.py
+++ experiments/config.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c264da2ff186df0b28e018a6adc1e815154408f1d013bd949ad69da76abfb695
-size 13305
+oid sha256:2a40da92ae5ef2edaa7923279ff57915dc57be1f9fd130462c7ac2f9d546d514
+size 13628
--- experiments/scripts/train_prior.py
+++ experiments/scripts/train_prior.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c5f17de418c2a5dd2dc6ea431a7bfa63d81f5027827955147368636ae535b5a2
-size 3983
+oid sha256:3c0766737e17bdc72e3932dbf660e8652bd62eb18210330ee08cc370cc791885
+size 4189
