47db17cd4998a3204713c7ca4c655b5fff170dd8
Dan Steinberg <daniel.steinberg@data61.csiro.au>
2024-09-18 13:11:37 +1000

    better digits architectures

--- experiments/scripts/digits.py
+++ experiments/scripts/digits.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5d3407d22c132eda142e983031e76fc2848857486fd510a379cc9116b3168972
-size 11975
+oid sha256:e5b807dfcdef697c31d78eeb6c5a1e8538e4b091f3008bdc765a2ba81c1e7598
+size 11997
