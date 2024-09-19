b4fa0bbd28f1ba76498b8955ca907ef6edce5dcb
dsteinberg <dan.m.steinberg@gmail.com>
2024-09-19 22:01:03 +1000

    bigger transformer gfp

--- experiments/config.py
+++ experiments/config.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dcc9738612f12d95a40a2cc7e0dcc94bb13ad4c3c1e1b93a93e50bdff18be694
+oid sha256:3d2be3c42a728c40a6a3e7deba52479d55c35dbd3c5e836df9a3745469166c8f
 size 14022
