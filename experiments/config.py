14b3b4a2369ca14e6e22bc79748ca95ac9e01d6c
dsteinberg <dan.m.steinberg@gmail.com>
2024-09-20 07:37:15 +1000

    lower learning rate of GFP

--- experiments/config.py
+++ experiments/config.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3d2be3c42a728c40a6a3e7deba52479d55c35dbd3c5e836df9a3745469166c8f
+oid sha256:059533eda738d896a6a8fd079f9213f69ecce1697bb4fa2263934ec17997eca6
 size 14022
