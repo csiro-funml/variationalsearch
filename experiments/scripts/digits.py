c8f3c0ab9485488377898522e4f26abd6a7d7138
dsteinberg <dan.m.steinberg@gmail.com>
2024-09-20 07:49:09 +1000

    more robust CPE for digits

--- experiments/scripts/digits.py
+++ experiments/scripts/digits.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1350af8e64984f171a3bbcd0acbed417bf47b0792cf391969f428f5408b6b743
-size 12352
+oid sha256:93f92a8d41f35a6149764c933a79a17f1393c9da8040e120ae0a6ecbfe8e9627
+size 12351
