514518c2b0a82eee5697f929a8e6116366591d9f
dsteinberg <dan.m.steinberg@gmail.com>
2024-09-19 14:32:35 +1000

    GFP transformer with gradient clipping

--- experiments/config.py
+++ experiments/config.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6537e196af2df0b09e84d28b0f459446107c671df319be98a9370c4acaa2ee92
-size 13937
+oid sha256:1fb367b05ff705316ee7b61c87b2f2cca7d0a3e301ed9e76b8e31f324a08b5c0
+size 14023
