c211d8a9a08e44c5d3af643657831c56e6add270
dsteinberg <dan.m.steinberg@gmail.com>
2024-09-19 20:39:42 +1000

    remove debugging code

--- vsd/generation.py
+++ vsd/generation.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5bb1012f6fba33165b14bc2eb39eec8f20a8ce27bb60cfa3458e705e0d0c4cb7
-size 17725
+oid sha256:a6a8708aece06802ab2ac411a66d6e47e0aabd85fe7644ec19354424a09a0978
+size 17684
