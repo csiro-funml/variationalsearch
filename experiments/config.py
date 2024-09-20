482bc13b9ebf85564f11e31e26bfab3c8743c280
dsteinberg <dan.m.steinberg@gmail.com>
2024-09-20 14:19:21 +1000

    new AAV prior

--- experiments/config.py
+++ experiments/config.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f789ddfdac7cfe49a24b68e6b58624cc83901ea856c0b32ca85dd7bf71041ffa
-size 14245
+oid sha256:b0ff12509a1ffeba25ad8e19eb48455b156b7cb69709737cbf42b7efc5a7f78f
+size 14333
