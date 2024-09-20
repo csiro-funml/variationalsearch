429f21a362273c13ff53fd10c3a92311df851f5e
dsteinberg <dan.m.steinberg@gmail.com>
2024-09-20 16:23:17 +1000

    GFP MC redux

--- experiments/config.py
+++ experiments/config.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b0ff12509a1ffeba25ad8e19eb48455b156b7cb69709737cbf42b7efc5a7f78f
-size 14333
+oid sha256:1ba42bf58db333f5bae55cfa471317adad4893d25147a3a6ffd0b2921151551a
+size 14413
