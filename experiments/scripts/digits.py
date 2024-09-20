380109304fcab9ba24e9fadccada81c401c65d61
dsteinberg <dan.m.steinberg@gmail.com>
2024-09-21 09:13:18 +1000

    more samples from digits proposals

--- experiments/scripts/digits.py
+++ experiments/scripts/digits.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fbddbc17733acdce7b5a53a366d4c16902cfec64505ef796ed524dd140cc06db
-size 12416
+oid sha256:c6f237653b04733b2407b78a68df76c5a2c8a6571b2c547055cfb5b083ef9213
+size 12417
