41015ae5620204f59aeea474dd3fa99c4898e205
dsteinberg <dan.m.steinberg@gmail.com>
2024-09-20 11:44:23 +1000

    new AAV and GFP priors

--- experiments/config.py
+++ experiments/config.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:66f21a8af7afb9f540fc6d9e35f5d46d12dfbd777b45226c0d535115e7505892
-size 14154
+oid sha256:f789ddfdac7cfe49a24b68e6b58624cc83901ea856c0b32ca85dd7bf71041ffa
+size 14245
